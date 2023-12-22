b4bbb2a65924f70d6c0db85f33fe4011965aa9df
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2023-12-22 14:26:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,16 +5,15 @@ from pymongo import MongoClient
 from pyrogram import Client, filters
 from apscheduler.schedulers.asyncio import AsyncIOScheduler  # Import the scheduler
 
-# Load configuration from config.py
-API_ID = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_URI = os.getenv("MONGO_URI")
-ADMIN_USER_IDS = list(map(int, os.getenv("ADMIN_USER_IDS").split(",")))
-LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID"))
-DB_CHANNEL_ID = int(os.getenv("DB_CHANNEL_ID"))
-DOMAIN = os.getenv("DOMAIN")
-START_TEXT = os.getenv("START_TEXT")
+API_ID= 16536417
+API_HASH= "f6e58a549da642d7b765744a2f82c6d9"
+BOT_TOKEN= "6626911044:AAGiOxjN-ad_sjd0wN19pXGCaXngHA_J5pM"
+MONGO_URI= "mongodb+srv://trumbot:trumbot@cluster0.cfkaeno.mongodb.net/?retryWrites=true&w=majority"
+ADMIN_USER_IDS = set(int(x) for x in os.environ.get("ADMIN_USER_IDS", "563896360 921365334").split())
+LOG_CHANNEL_ID= -1001623353378  
+DB_CHANNEL_ID= -1001623353378  
+DOMAIN= "https://telegram.dog"
+START_TEXT= "Fuck You" 
 
 app = Client(
     "your_bot",
